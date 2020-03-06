996a767612049207bd43848a59a46d11a0698bdc
efimtceva <efimtceva@gmail.com>
2020-03-06 09:50:53 +0300

    Demo change

--- Tailspin.SpaceGame.Database/dbo/Tables/Profile.sql
+++ Tailspin.SpaceGame.Database/dbo/Tables/Profile.sql
@@ -5,3 +5,18 @@
     [favoriteMap]       NVARCHAR(50) NULL,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
+
+/*
+UPDATE DBO.Profiles
+SET favoriteMap='Milky Way'
+WHERE CAST((id) AS int) % 3 = 0
+GO
+UPDATE DBO.Profiles
+SET favoriteMap='Andromeda'
+WHERE CAST((id) AS int) % 3 = 1
+GO
+UPDATE DBO.Profiles
+SET favoriteMap='Pinwheel'
+WHERE CAST((id) AS int) % 3 = 2
+GO 
+*/
